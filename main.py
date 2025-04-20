ff21d93199e6f7ebaf493efec05853404b58c17d
hikuohiku <hikuohiku@gmail.com>
2025-04-20 19:04:57 +0900

    損失関数、最適化

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from dotenv import dotenv_values
+from torch import nn, optim
 
 from load_dataset import load_mnist
 from models import MLP
@@ -8,6 +9,7 @@ class Config:
     def __init__(self, env):
         self.batch_size = int(env["batch_size"] or "64")
         self.test_batch_size = int(env["test_batch_size"] or "32")
+        self.lr = float(env["lr"] or "0.1")
 
 
 def main(config: Config):
@@ -17,6 +19,10 @@ def main(config: Config):
     # モデルの定義
     model = MLP()
     print(model)
+
+    # 損失関数と最適化手法の定義
+    criterion = nn.CrossEntropyLoss()
+    optimizer = optim.SGD(model.parameters(), lr=config.lr)
 
 
 if __name__ == "__main__":
